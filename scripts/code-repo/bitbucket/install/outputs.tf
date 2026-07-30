7039a6891aae31bed7401a0abdb21744c2a52cf0
Federico Maleh <fedemaleh@yahoo.com.ar>
2026-07-30 09:34:24 -0300

    chore(code-repo): drop the temporary bitbucket spec installer

--- scripts/code-repo/bitbucket/install/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "specification_id" {
-  description = "ID of the bitbucket-configuration provider specification. This is the UUID that scripts/code-repo/create_code_repository maps `bitbucket` to."
-  value       = module.bitbucket_spec.specification_id
-}
-
-output "slug" {
-  description = "Slug of the specification, resolved from the rendered template (bitbucket-configuration)."
-  value       = module.bitbucket_spec.slug
-}
